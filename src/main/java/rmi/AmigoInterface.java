5b866984b4077053f9c65c40fa182dc7beceb809
Guilherme Mitsuo Honda <guihonda10@gmail.com>
2025-11-16 23:43:23 -0300

    feat: adicao dos comentarios mo AmigoInterface

--- src/main/java/rmi/AmigoInterface.java
+++ src/main/java/rmi/AmigoInterface.java
@@ -6,9 +6,15 @@ import java.util.List;
 import modelo.Amigo;
 
 public interface AmigoInterface extends Remote {
+    
+    /* Cadastra um novo amigo no sistema. */
     void cadastrarAmigo(Amigo amigo) throws RemoteException;
+    /* Atualiza os dados de um amigo já existente. */
     void atualizarAmigo(Amigo amigo) throws RemoteException;
+   /* Exclui um amigo do sistema pelo seu ID. */ 
     void excluirAmigo(int id) throws RemoteException;
+    /*Busca um amigo pelo seu ID. */
     Amigo buscarAmigoPorId(int id) throws RemoteException;
+     /*Retorna uma lista de todos os amigos cadastrados no sistema. */
     List<Amigo> listarAmigos() throws RemoteException;
 }
